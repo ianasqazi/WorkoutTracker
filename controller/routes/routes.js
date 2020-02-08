e0e180c54385fb15e3046747ddf26409680d1df6
Anas Qazi <ianasqazi@gmail.com>
2020-02-07 21:48:49 -0500

    insert working

--- controller/routes/routes.js
+++ controller/routes/routes.js
@@ -1,58 +1,6 @@
-// // ROUTING : 
-
-// const express = require("express");
-// const path = require("path");
-// const db = require("../../models");
-// // const Workout = require('../../models/workout');
-// const Workout = require('../../models/workout');
-
-// var router = express.Router();
-
-// ///
-
-
-// router.get("/", (req,res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// router.get("/exercise", (req,res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
-// });
-
-// router.get("/stats", (req,res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'stats.html'));
-// });
-
-// router.get("/api/workouts", (req,res) => {
-//   db.Workout.find({}).sort({day:-1}).limit(1)
-//   .then(result => {
-//     res.json(result);
-//   })
-//   .catch(err => {
-//     res.json(err);
-//   });
-// });
-
-// router.get('/api/workouts/range', (req, res) => {
-//   db.Workout.find({})
-//   .then(result => {
-//     res.json(result);
-//   })
-//   .catch(err => {
-//     res.json(err);
-//   });
-
-// });
-
-// module.exports = router;
-
-
-
 const express = require("express");
 const path = require("path");
 
-// var htmlRoutes = express.Router();
-// var apiRoutes = express.Router();
 var routes = express.Router();
 
 const db = require("../../models");
